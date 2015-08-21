7bf0eaf1b63e8451621878729b56bed1ca1936e8
Deep Mehta <mehtadeep118@gmail.com>
2015-08-21 14:15:18 -0700

    finish Arrays

--- CrackingCodingInterview/src/LinkedIst/LinkedList.java
+++ CrackingCodingInterview/src/LinkedIst/LinkedList.java
@@ -9,6 +9,48 @@ package LinkedIst;
  *
  * @author deep
  */
-public class LinkedList {
+public class LinkedList 
+{
+    public static void main(String[] args)
+    {
+        Node x = new Node(1);
+        x.appendtoTail(2);
+        x.appendtoTail(3);
+        x.appendtoTail(4);
+        printLinkedList(x);
+    }
+    
+    public static void printLinkedList(Node head)
+    {
+     while(head!=null)
+     {
+         System.out.format("%d ", head.val);  
+            head = head.next; 
+     }
+     System.out.println();
+    }
+}
+class Node
+{
+    Node next;
+    int val;
+    Node(int x)
+    {
+       val = x;
+       next=null;
+    }
+    
+    void appendtoTail(int d)
+    {
+       Node end = new Node(d);
+       Node n = this;
+       while(n.next!=null)
+       {
+           n=n.next;
+       }
+       n.next=end;
+        
+    }
+    
     
 }

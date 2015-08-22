e275f91c464bc4307ddce36f6e242576c5b0ef08
mehtadeep118 <mehtadeep118@gmail.com>
2015-08-22 00:17:13 -0700

    Start New

--- CrackingCodingInterview/src/LinkedIst/RemoveDupUnsortedLinkedList.java
+++ CrackingCodingInterview/src/LinkedIst/RemoveDupUnsortedLinkedList.java
@@ -5,6 +5,8 @@
  */
 package LinkedIst;
 
+import java.util.HashSet;
+
 /**
  *
  * @author deep
@@ -13,7 +15,31 @@ public class RemoveDupUnsortedLinkedList
 {
     public static void main(String args[])
     {
+        Node x = new Node(1);
+        x.next=new Node(9);
+        x.next.next=new Node(2);
+        x.next.next.next=new Node(12);
+        x.next.next.next.next=new Node(2);
         
+    }
+    public static void removeDuplicates(Node x)
+    {
+        HashSet <Integer> set = new HashSet <Integer> ();
+        Node previous = null;
+        
+        while(x!=null)
+        {
+            if(set.contains(x.val))
+            {
+                previous.next=x.next;
+            }
+            else
+            {
+                set.add(x.val);
+                previous = x;
+            }
+            x=x.next;
+        }
     }
     
 }

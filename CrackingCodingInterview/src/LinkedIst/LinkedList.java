30fc8a16e0e59d15fc17bde5e0b72d1c53ff9df0
Deep Mehta <mehtadeep118@gmail.com>
2015-08-21 18:12:33 -0700

    finish Arrays

--- CrackingCodingInterview/src/LinkedIst/LinkedList.java
+++ CrackingCodingInterview/src/LinkedIst/LinkedList.java
@@ -13,10 +13,18 @@ public class LinkedList
 {
     public static void main(String[] args)
     {
+        /*
         Node x = new Node(1);
         x.appendtoTail(2);
         x.appendtoTail(3);
         x.appendtoTail(4);
+        printLinkedList(x);
+        */
+        
+        Node x = new Node(1);
+        x.next=new Node(2);
+        x.next.next=new Node(3);
+        x.next.next.next=new Node(10);
         printLinkedList(x);
     }
     
@@ -29,28 +37,4 @@ public class LinkedList
      }
      System.out.println();
     }
-}
-class Node
-{
-    Node next;
-    int val;
-    Node(int x)
-    {
-       val = x;
-       next=null;
-    }
-    
-    void appendtoTail(int d)
-    {
-       Node end = new Node(d);
-       Node n = this;
-       while(n.next!=null)
-       {
-           n=n.next;
-       }
-       n.next=end;
-        
-    }
-    
-    
 }
---
+++ CrackingCodingInterview/src/LinkedIst/Node.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedIst;
+
+/**
+ *
+ * @author deep
+ */
+class Node
+{
+    Node next;
+    int val;
+    Node(int x)
+    {
+       val = x;
+       next=null;
+    }
+    
+    void appendtoTail(int d)
+    {
+       Node end = new Node(d);
+       Node n = this;
+       while(n.next!=null)
+       {
+           n=n.next;
+       }
+       n.next=end;
+        
+    }
+    
+    
+}

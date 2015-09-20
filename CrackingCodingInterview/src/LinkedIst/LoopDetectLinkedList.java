c9d3aa312e4b7815a844d68bf1bd1ed6ea4b8faa
mehtadeep118 <mehtadeep118@gmail.com>
2015-09-19 19:07:25 -0700

    adding code

--- CrackingCodingInterview/src/LinkedIst/LoopDetectLinkedList.java
+++ CrackingCodingInterview/src/LinkedIst/LoopDetectLinkedList.java
@@ -9,6 +9,34 @@ package LinkedIst;
  *
  * @author deep
  */
-public class LoopDetectLinkedList {
+public class LoopDetectLinkedList 
+{
+    public static void main(String[] args)
+    {
+        Node x = new Node(1);
+        x.next=new Node(2);
+        x.next.next=new Node(4);
+        x.next.next.next=new Node(6);
+        x.next.next.next.next=new Node(8);
+        x.next.next.next.next.next=new Node(10);
+        x.next.next.next.next.next.next=new Node(12);
+        System.out.println(cycle(x));
+    }
+    
+    public static boolean cycle(Node head)
+    {
+        Node fast=head;
+        Node slow=head;
+        
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            
+            if(fast==slow)
+                return true;
+        }
+        return false;
+    }
     
 }

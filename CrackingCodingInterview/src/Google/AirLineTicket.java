edfa905b18d3f27e1bcf77f25f6a0d444ef8ded2
mehtadeep118 <mehtadeep118@gmail.com>
2015-09-19 23:02:08 -0700

    adding code

--- CrackingCodingInterview/src/Google/AirLineTicket.java
+++ CrackingCodingInterview/src/Google/AirLineTicket.java
@@ -5,10 +5,49 @@
  */
 package Google;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /**
  *
  * @author Deep
+ * Given an bunch of airline tickets with [from, to], for example [MUC, LHR], [CDG, MUC], [SFO, SJC], [LHR, SFO], please reconstruct the itinerary in order, 
+   for example: [ CDG, MUC, LHR, SFO, SJC ]. 
+   tickets can be represented as nodes
  */
-public class AirLineTicket {
+public class AirLineTicket 
+{
+    public static void main(String[] args)
+    {
+        reconstruct();
+    }
     
+    public static void reconstruct()
+    {
+        String start="";
+        HashMap<String,String> map = new HashMap<String,String>();
+        map.put("MUC", "LHR");
+        map.put("CDG", "MUC");
+        map.put("SFO", "SJC");
+        map.put("LHR", "SFO");
+        TreeMap<String,String> tree = new TreeMap<String,String>(map);
+        TreeSet<String> set = new TreeSet<String>(map.values());
+        System.out.println(set);
+       // if(tree.containsValue(tree))
+        start=tree.firstKey();
+        System.out.println(start);
+        for(int i=0;i<tree.size();i++)
+        {
+            
+            System.out.print(" "+tree.get(start));
+            
+            start=tree.get(start);
+            
+           
+        }
+        
+        
+    }
 }
